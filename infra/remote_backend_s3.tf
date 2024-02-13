968ddc2f2a3bc67cddca8c3d9c19c671346544b2
yvreddy <yvreddydevops@gmail.com>
2024-02-13 20:31:34 +0000

    First Change

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-remote-state-bucket-yvr"
+    bucket = "dev-proj-1-remote-state-bucket"
     key    = "devops-project-1/terraform.tfstate"
     region = "eu-west-2"
   }
